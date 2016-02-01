e228c76e8f21cb7c5f879e118d4b808fbb48df83
benjh33 <bjameshunter@gmail.com>
2016-02-01 19:16:47 +0000

    misnamed jenkins docker images

--- identidock/jenkins.sh
+++ identidock/jenkins.sh
@@ -15,8 +15,8 @@ if [ $ERR -eq 0 ];then
     if [ $CODE -eq 200 ]; then
         echo "Tests passed - tagging images"
         HASH=$(git rev-parse --short HEAD)
-        sudo docker tag -f jenkins_identidock benjh33/identidock:$HASH
-        sudo docker tag -f jenkins_identidock benjh33/identidock:newest
+        sudo docker tag -f jenkins_identidock_1 benjh33/identidock:$HASH
+        sudo docker tag -f jenkins_identidock_1 benjh33/identidock:newest
         echo "Pushing to dockerhub"
         docker login -u benjh33
         docker push benjh33/identidock:$HASH
