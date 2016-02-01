e0027f7c7feedd74194ab0e2aede5a91e87ecfb3
benjh33 <bjameshunter@gmail.com>
2016-02-01 19:12:01 +0000

    added to identidock script so pushes to dockerhub

--- identidock/jenkins.sh
+++ identidock/jenkins.sh
@@ -12,7 +12,16 @@ ERR=$
 if [ $ERR -eq 0 ];then
     IP=$(sudo docker inspect -f {{ .NetworkSettings.IPAddress }} jenkins_identidock_1)
     CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
-    if [ $CODE -ne 200 ]; then
+    if [ $CODE -eq 200 ]; then
+        echo "Tests passed - tagging images"
+        HASH=$(git rev-parse --short HEAD)
+        sudo docker tag -f jenkins_identidock benjh33/identidock:$HASH
+        sudo docker tag -f jenkins_identidock benjh33/identidock:newest
+        echo "Pushing to dockerhub"
+        docker login -u benjh33
+        docker push benjh33/identidock:$HASH
+        docker push benjh33/identidock:newest
+    else
         echo "Site returned " $CODE
         ERR=1
     fi
